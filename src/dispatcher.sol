6975f20f533f714b5635d216bd6d984a870b7da0
Daniel Ellison <daniel@syrinx.net>
2017-01-12 14:41:09 -0500

    Completed reorganization and code updating.

--- src/dispatcher.sol
+++ src/dispatcher.sol
@@ -1,16 +1,12 @@
-contract Dispatcher {
+pragma solidity ^0.4.0;
 
-    // Functions.
+contract Dispatcher {
     function Dispatcher(address contractAddress);
     function initialize() returns (bool result);
     function replace(address newContract) returns (address newAddress);
     function double(uint256 multiplicand) returns (uint256 product);
     function halve(uint256 dividend) returns (uint256 quotient);
     
-    // Events.
     event Initialized(bool result);
     event Replaced(address indexed oldAddress, address newAddress);
-    event Doubled(uint256 product);
-    event Halved(uint256 quotient);
-    
 }
